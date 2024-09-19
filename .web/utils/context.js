018d6064b03be34b792fe15b6e68af25766c6b8a
Daniel Banariba <danielbanariba@protonmail.com>
2024-09-19 14:05:34 -0600

    Yo no se que pedos aqui pero aja

--- .web/utils/context.js
+++ .web/utils/context.js
@@ -20,7 +20,7 @@ export const initialEvents = () => []
 
 export const isDevMode = true
 
-export const lastCompiledTimeStamp = "2024-09-11 00:15:40.183726"
+export const lastCompiledTimeStamp = "2024-09-19 14:02:00.210139"
 
 export function UploadFilesProvider({ children }) {
   const [filesById, setFilesById] = useState({})
