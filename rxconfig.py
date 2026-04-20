d8ff86a702ff5b68f967b43e697b68fe9725574b
Daniel Banariba <banaribad@gmail.com>
2026-04-19 23:09:49 -0600

    feat: hybrid deploy - static on Vercel + backend on app subdomain

--- rxconfig.py
+++ rxconfig.py
@@ -5,7 +5,11 @@ config = rx.Config(
     app_name="links_bio",
     db_url="sqlite:///reflex.db",
     disable_plugins=[SitemapPlugin],
-    api_url="https://danielbanariba.com",
+    api_url="https://app.danielbanariba.com",
     deploy_url="https://danielbanariba.com",
-    cors_allowed_origins=["https://danielbanariba.com", "https://www.danielbanariba.com"],
+    cors_allowed_origins=[
+        "https://danielbanariba.com",
+        "https://www.danielbanariba.com",
+        "https://app.danielbanariba.com",
+    ],
 )
